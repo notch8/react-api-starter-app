8fd565bd24d006a40d6ad5d87bdfd575e0301244
MATTHEW CLARK <matt@notch8.com>
2017-05-21 07:35:50 -0700

    cleans up app.js for start

--- src/App.js
+++ src/App.js
@@ -2,31 +2,12 @@ import React, { Component } from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './routes/Home'
-import UserStore from './stores/UserStore'
 import UserDetail from './routes/UserDetail'
 import UserRegistration from './routes/UserRegistration'
 import Login from './routes/Login'
 
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.setUser = this.setUser.bind(this)
-  }
-
-  componentWillMount(){
-    UserStore.on('change', this.setUser)
-  }
-
-  componentWillUnmount(){
-    UserStore.removeListener('change', this.setUser)
-  }
-
-  setUser(user){
-    this.setState({user: user})
-    console.log('user has been set')
-  }
-
   render() {
     return (
       <Router>
